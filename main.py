4ed0c8b46b0de1757be87c5299996b763e088c3d
student06 <jakub.gola2@teb.edu.pl>
2025-03-06 14:22:48 +0100

    06.03.2025

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from PyQt6.QtWidgets import QDialog
 
+from layout import Ui_Dialog
+
 
 class MyForm(QDialog):
-    self
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.show()
+
+    def
