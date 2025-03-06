3b0dbdb20ec3797df8b5c3b49e2fa21ca0490773
kubix <jakub.gola2@teb.edu.pl>
2025-03-06 22:20:14 +0100

    06.03.2025 - home

--- person.py
+++ person.py
@@ -1,6 +1,3 @@
-from PyQt6.QtGui.QRawFont import weight
-
-
 class Address:
     def __init__(self, post1, post2):
         self.post1 = post1
@@ -17,12 +14,9 @@ class Person:
     def validate_pesel(self):
         if self.id.isdigit() and len(self.id) == 11:
             weight = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
-            sum = sum()
-            rest = (sum % 10) - 10
-            control_number = 0
-            if rest == 10:
-                control_number = 0
-            else:
-                s
+            sum = sum(int(id[i]) * weight[i] for i in range(10))
+            rest = (10 - (sum % 10)) % 10
+            return rest == int(id[10])
 
-        raise ValueError("Invalid pesel")
+        else:
+            raise ValueError("Invalid pesel")
