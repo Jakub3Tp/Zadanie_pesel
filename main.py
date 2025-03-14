52df23039457a4f9ff7f20efe205c26b4d0586d8
Jakub4Tp <154210380+Jakub3Tp@users.noreply.github.com>
2025-03-14 13:24:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,17 +13,23 @@ class MyForm(QDialog):
         self.ui.save.clicked.connect(self.saving)
         self.show()
 
-    def saving(self):
+        def saving(self):
         name = self.ui.name.text()
         surname = self.ui.surname.text()
-        birth = self.ui.birth.text()
-        id = self.ui.id.text()
+        birth = self.ui.dateEdit.date().toPyDate()
+        pesel = self.ui.pesel.text()
         post1 = self.ui.post1.text()
         post2 = self.ui.post2.text()
 
-        with open('./data.txt', 'w+') as file:
-            file.write(f'{name}, {surname}, {birth}, {id}, {post1}, {post2} \n')
+        try:
+            self.persons.persons.append(Person(name, surname, birth, post1, post2, pesel))
+        except ValueError as e:
+            message = QMessageBox()
+            message.setText(e.__str__())
+            message.exec()
 
+        #with open('./data.txt', 'w+') as file:
+        #    file.write(f'{name}, {surname}, {birth}, {pesel}, {post1}, {post2} \n')
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MyForm()
